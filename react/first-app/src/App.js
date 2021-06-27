c6baed77a0deccff2efd0cf89fb9b2bbc4d2c484
Pranav Rajan <rajan.pranav320@gmail.com>
2021-06-27 14:45:31 -0600

    add react

---
+++ react/first-app/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Hello, World!</h2>
+      {/* <p>This is a visible paragraph</p> */}
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
